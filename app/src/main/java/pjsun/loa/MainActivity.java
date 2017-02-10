370725803603fcc4fb432baa294d6388ee72d0a7
sunpingji <sunpingji@126.com>
2017-02-10 14:49:35 +0800

    add all needed

--- app/src/main/java/pjsun/loa/MainActivity.java
+++ app/src/main/java/pjsun/loa/MainActivity.java
@@ -1,13 +1,103 @@
 package pjsun.loa;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopScoreDocCollector;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.Version;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+import static junit.framework.Assert.assertEquals;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_index).setOnClickListener(this);
+        findViewById(R.id.btn_search).setOnClickListener(this);
+        tvResult = (TextView) findViewById(R.id.tv_result);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_index:
+                try {
+                    addIndex();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.btn_search:
+//                try {
+//                    search();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                } catch (org.apache.lucene.queryparser.classic.ParseException e) {
+//                    e.printStackTrace();
+//                }
+                break;
+        }
+    }
+
+
+    private void addIndex() throws IOException {
+        Analyzer analyzer = new StandardAnalyzer();
+
+        // Store the index in memory:
+        Directory directory = new RAMDirectory();
+        // To store an index on disk, use this instead:
+        //Directory directory = FSDirectory.open("/tmp/testindex");
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        IndexWriter iwriter = new IndexWriter(directory, config);
+        Document doc = new Document();
+        String text = "This is the text to be indexed.";
+        doc.add(new Field("fieldname", text, TextField.TYPE_STORED));
+        iwriter.addDocument(doc);
+        iwriter.close();
+    }
+
+//    private void search() throws IOException, org.apache.lucene.queryparser.classic.ParseException {
+//        DirectoryReader ireader = DirectoryReader.open(directory);
+//        IndexSearcher isearcher = new IndexSearcher(ireader);
+//        // Parse a simple query that searches for "text":
+//        QueryParser parser = new QueryParser("fieldname", analyzer);
+//        Query query = parser.parse("text");
+//        ScoreDoc[] hits = isearcher.search(query, null, 1000).scoreDocs;
+//        assertEquals(1, hits.length);
+//        // Iterate through the results:
+//        for (int i = 0; i < hits.length; i++) {
+//            Document hitDoc = isearcher.doc(hits[i].doc);
+//            assertEquals("This is the text to be indexed.", hitDoc.get("fieldname"));
+//        }
+//        ireader.close();
+//        directory.close();
+//    }
+
 }
