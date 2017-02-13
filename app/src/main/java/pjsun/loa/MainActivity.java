22dac395a2da3dceead86758026188e3f2f2dfca
sunpingji <sunpingji@126.com>
2017-02-13 14:07:16 +0800

    adjust

--- app/src/main/java/pjsun/loa/MainActivity.java
+++ app/src/main/java/pjsun/loa/MainActivity.java
@@ -3,33 +3,26 @@ package pjsun.loa;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
-import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopScoreDocCollector;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
-import org.apache.lucene.util.Version;
 
 import java.io.IOException;
-import java.text.ParseException;
 
-import static junit.framework.Assert.assertEquals;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -55,23 +48,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
             case R.id.btn_search:
-//                try {
-//                    search();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                } catch (org.apache.lucene.queryparser.classic.ParseException e) {
-//                    e.printStackTrace();
-//                }
+                try {
+                    search();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (org.apache.lucene.queryparser.classic.ParseException e) {
+                    e.printStackTrace();
+                }
                 break;
         }
     }
 
+    private Directory directory;
+    private  Analyzer analyzer;
+
 
     private void addIndex() throws IOException {
-        Analyzer analyzer = new StandardAnalyzer();
+         analyzer = new StandardAnalyzer();
 
         // Store the index in memory:
-        Directory directory = new RAMDirectory();
+         directory = new RAMDirectory();
         // To store an index on disk, use this instead:
         //Directory directory = FSDirectory.open("/tmp/testindex");
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
@@ -83,21 +79,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         iwriter.close();
     }
 
-//    private void search() throws IOException, org.apache.lucene.queryparser.classic.ParseException {
-//        DirectoryReader ireader = DirectoryReader.open(directory);
-//        IndexSearcher isearcher = new IndexSearcher(ireader);
-//        // Parse a simple query that searches for "text":
-//        QueryParser parser = new QueryParser("fieldname", analyzer);
-//        Query query = parser.parse("text");
-//        ScoreDoc[] hits = isearcher.search(query, null, 1000).scoreDocs;
-//        assertEquals(1, hits.length);
-//        // Iterate through the results:
-//        for (int i = 0; i < hits.length; i++) {
-//            Document hitDoc = isearcher.doc(hits[i].doc);
-//            assertEquals("This is the text to be indexed.", hitDoc.get("fieldname"));
-//        }
-//        ireader.close();
-//        directory.close();
-//    }
+    private void search() throws IOException, org.apache.lucene.queryparser.classic.ParseException {
+        DirectoryReader ireader = DirectoryReader.open(directory);
+        IndexSearcher isearcher = new IndexSearcher(ireader);
+        // Parse a simple query that searches for "text":
+        QueryParser parser = new QueryParser("fieldname", analyzer);
+        Query query = parser.parse("text");
+        ScoreDoc[] hits = isearcher.search(query, null, 1000).scoreDocs;
+
+        // Iterate through the results:
+        for (int i = 0; i < hits.length; i++) {
+            Document hitDoc = isearcher.doc(hits[i].doc);
+        }
+        ireader.close();
+        directory.close();
+    }
 
 }
