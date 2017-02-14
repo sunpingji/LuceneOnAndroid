937fe72eb7d50f2ae38465ff957c9bba900c66c7
sunpingji <pingji.spj@gmail.com>
2017-02-14 16:04:18 +0800

    use lucene 3.6.2

--- app/src/main/java/pjsun/loa/MainActivity.java
+++ app/src/main/java/pjsun/loa/MainActivity.java
@@ -9,19 +9,23 @@ import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
-import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.DirectoryReader;
+
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.queryparser.classic.QueryParser;
+
+import org.apache.lucene.queryParser.ParseException;
+
+import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.Version;
 
 import java.io.IOException;
 
+import static junit.framework.Assert.assertEquals;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -41,20 +45,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_index:
+
                 try {
                     addIndex();
                 } catch (IOException e) {
                     e.printStackTrace();
+                } catch (ParseException e) {
+                    e.printStackTrace();
                 }
+
                 break;
             case R.id.btn_search:
+
                 try {
                     search();
                 } catch (IOException e) {
                     e.printStackTrace();
-                } catch (org.apache.lucene.queryparser.classic.ParseException e) {
+                } catch (ParseException e) {
                     e.printStackTrace();
                 }
+
                 break;
         }
     }
@@ -63,34 +73,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private  Analyzer analyzer;
 
 
-    private void addIndex() throws IOException {
-         analyzer = new StandardAnalyzer();
+    private void addIndex() throws IOException, ParseException {
+        Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_CURRENT);
 
         // Store the index in memory:
-         directory = new RAMDirectory();
+        Directory directory = new RAMDirectory();
         // To store an index on disk, use this instead:
         //Directory directory = FSDirectory.open("/tmp/testindex");
-        IndexWriterConfig config = new IndexWriterConfig(analyzer);
-        IndexWriter iwriter = new IndexWriter(directory, config);
+        IndexWriter iwriter = new IndexWriter(directory, analyzer, true,
+                new IndexWriter.MaxFieldLength(25000));
         Document doc = new Document();
         String text = "This is the text to be indexed.";
-        doc.add(new Field("fieldname", text, TextField.TYPE_STORED));
+        doc.add(new Field("fieldname", text, Field.Store.YES,
+                Field.Index.ANALYZED));
         iwriter.addDocument(doc);
         iwriter.close();
-    }
 
-    private void search() throws IOException, org.apache.lucene.queryparser.classic.ParseException {
-        DirectoryReader ireader = DirectoryReader.open(directory);
+        // Now search the index:
+        IndexReader ireader = IndexReader.open(directory); // read-only=true
         IndexSearcher isearcher = new IndexSearcher(ireader);
         // Parse a simple query that searches for "text":
-        QueryParser parser = new QueryParser("fieldname", analyzer);
+        QueryParser parser = new QueryParser(Version.LUCENE_CURRENT,"fieldname", analyzer);
         Query query = parser.parse("text");
         ScoreDoc[] hits = isearcher.search(query, null, 1000).scoreDocs;
-
+        assertEquals(1, hits.length);
         // Iterate through the results:
         for (int i = 0; i < hits.length; i++) {
             Document hitDoc = isearcher.doc(hits[i].doc);
+            assertEquals("This is the text to be indexed.", hitDoc.get("fieldname"));
         }
+        isearcher.close();
+        ireader.close();
+        directory.close();
+    }
+
+    private void search() throws IOException, ParseException {
+        IndexReader ireader = IndexReader.open(directory); // read-only=true
+        IndexSearcher isearcher = new IndexSearcher(ireader);
+        // Parse a simple query that searches for "text":
+        QueryParser parser = new QueryParser(Version.LUCENE_CURRENT,"fieldname", analyzer);
+        Query query = parser.parse("text");
+        ScoreDoc[] hits = isearcher.search(query, null, 1000).scoreDocs;
+        assertEquals(1, hits.length);
+        // Iterate through the results:
+        for (int i = 0; i < hits.length; i++) {
+            Document hitDoc = isearcher.doc(hits[i].doc);
+            assertEquals("This is the text to be indexed.", hitDoc.get("fieldname"));
+        }
+        isearcher.close();
         ireader.close();
         directory.close();
     }
